38cb54c08b77a92759587b3bf63437a300735494
Piotr <ptk.siewiera@gmail.com>
2023-01-05 19:40:22 +0100

    Shift_rows added

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-def aes_encrypt(plaintext, rounds):
-    # add round key
-    for i in (0, rounds):
-        # substitute bytes
-        # shift rows
-        # mix columns
-        # add round key
+# def aes_encrypt(plaintext, rounds):
+#     # add round key
+#     for i in (0, rounds):
+#         sub_bytes() # substitute bytes
+#         # shift rows
+#         # mix columns
+#         # add round key
+#
+# def sub_bytes():
+
+def shift_rows(array):
+    n = 0
+    shifted = []
+    for row in array:
+        row = row[n:] + row[:n]
+        n=n+1
+        shifted.append(row)
+    return shifted
+
+array = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+rot = shift_rows(array)
+for row in rot:
+    print(row)
 
